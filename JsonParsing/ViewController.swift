ffb3d1e33c97b3bff340ca9f963e32e96ae7db6f
pratamawijaya <set.mnemonix@gmail.com>
2017-12-10 01:22:38 +0700

    Initial Commit

---
+++ JsonParsing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JsonParsing
+//
+//  Created by Pratama Nur Wijaya on 10/12/17.
+//  Copyright © 2017 Pratama Nur Wijaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
