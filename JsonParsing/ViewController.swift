69c97e9689e39734c96ad0861c7dc6cb283eddf5
pratamawijaya <set.mnemonix@gmail.com>
2017-12-10 01:48:58 +0700

    add code

--- JsonParsing/ViewController.swift
+++ JsonParsing/ViewController.swift
@@ -8,11 +8,43 @@
 
 import UIKit
 
+struct VipBitcoin: Decodable {
+    public let ticker: Ticker
+}
+
+struct Ticker: Decodable {
+    let high :String
+    let low :String
+    let last: String
+    let buy: String
+    let sell: String
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let jsonURL = "https://vip.bitcoin.co.id/api/ltc_idr/ticker"
+        
+        guard let url = URL(string: jsonURL) else
+          { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+            // check err
+            // check response 200
+            guard let data = data else {return}
+            do{
+                let data = try JSONDecoder().decode(VipBitcoin.self, from: data)
+                print(data.ticker.high)
+                print(data.ticker.low)
+                print(data.ticker.last)
+            }catch let jsonErr {
+                print(jsonErr.localizedDescription)
+            }
+           
+        }.resume()
+        
     }
 
     override func didReceiveMemoryWarning() {
